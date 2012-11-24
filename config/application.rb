5c9f7c0e3f5839bfc05d4824c3ee27ec3cb91ba3
Ubuntu <ubuntu@ip-10-150-130-78.ap-northeast-1.compute.internal>
2012-11-24 19:58:24 +0000

    check all errors in AWS

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module KnuLibrarySearch
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.precompiled = true
+    config.assets.compiled = true
   end
 end
